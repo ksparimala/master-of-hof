4562b1535c750f96dcf20e87c1d2a10e09993704
ksparimala <parimala@DESKTOP-QIPH3QC.localdomain>
2020-11-19 02:07:07 +0530

    done

---
+++ hof/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HigherOrderComponent from './components/hof/HigherOrderComponent';
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HigherOrderComponent />
+    </div>
+  );
+}
+export default App
